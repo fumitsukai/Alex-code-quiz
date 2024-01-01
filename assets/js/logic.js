dbcc98a273aea1f11db321de56d484b676ac2be2
Alexandru Pandelea <alexandrupandelea@Alexandrus-MacBook-Pro.local>
2024-01-01 12:43:01 +0000

    added array of questions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,8 +2,8 @@ const startScreen = document.querySelector("#start-screen");
 const startBtn = document.querySelector("#start");
 const timeEl = document.querySelector("#timer");
 
-//make landing page dissapear once the start button is clicked
 
+//make landing page dissapear once the start button is clicked
 start.addEventListener("click", () => {
     startScreen.style.display = "none";
 });
---
+++ assets/js/questions.js
@@ -0,0 +1,46 @@
+//declare an array of objects for questions
+
+const questions = [
+    {
+        question: "How many turkeys were served at Harry's first Hogwarts Christmas dinner in the Great Hall?",
+        answerOne: "Ten",
+        answerTwo: "One Hundred",
+        answerThree: "Seventy",
+        answerFour: "Fifty",
+        correctAnswer: "One Hundred"
+    },
+    {
+        question: "Where did Harry have Christmas Dinner in his fifth year?",
+        answerOne: "Grimmauld Place",
+        answerTwo: "Hogwarts",
+        answerThree: "Privet Drive",
+        answerFour: "The Burrow",
+        correctAnswer: "Grimmauld Place"
+    },
+    {
+        question: "When the Christmas crackers were pulled in the Philosopher's Stone, who ended up with a lovely, flowered bonnet?",
+        answerOne: "Argus Filch",
+        answerTwo: "Albus Dumbledore",
+        answerThree: "Madam Pince",
+        answerFour: "Minerva McGonagall",
+        correctAnswer: "Albus Dumbledore"
+    },
+    {
+        question: "What was special about the Christmas dinner in Harry's forth year?",
+        answerOne: "The feast was held during the Yule Ball",
+        answerTwo: "The Triwizard champions were announced after the feast",
+        answerThree: "It was the first Christmas Harry spent at The Burrow",
+        answerFour: "The first Triwizard task took place after the feast",
+        correctAnswer: "The feast was held during the Yule Ball"
+    },
+    {
+        question: "Who interrupted Christmas dinner at The Burrow with their unplanned visit?",
+        answerOne: "Ted and Andromeda Tonks",
+        answerTwo: "Amos Diggory and Kingsley Shacklebolt",
+        answerThree: "Percy Weasley and Rufus Scrimgeor",
+        answerFour: "Cornelius Fudge and Percy Weasley",
+        correctAnswer: "Percy Weasley and Rufus Scrimgeor"
+    }                      
+];
+
+console.log(questions[0]);
